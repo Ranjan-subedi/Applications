7eb1c61b8e0a0c9e3091af80ab95f2e6ad14662e
Ranjan Subedi <144402172+Ranjan-subedi@users.noreply.github.com>
2025-12-08 18:36:05 +0545

    tic_added

--- tic_tac_toe/lib/main.dart
+++ tic_tac_toe/lib/main.dart
@@ -10,10 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Tic_Tac_Toe(),
+      home:const Tic_Tac_Toe(),
     );
   }
 }
+
+
 
 class Tic_Tac_Toe extends StatefulWidget {
   const Tic_Tac_Toe({super.key});
@@ -23,26 +25,57 @@ class Tic_Tac_Toe extends StatefulWidget {
 }
 
 class _Tic_Tac_ToeState extends State<Tic_Tac_Toe> {
+  String lastValue = "X";
+  List boardData = ["", "", "", "", "", "", "", "", ""]; 
+
+  changeValue(){
+    if(lastValue == "X"){
+      lastValue = "O";
+    }else{
+      lastValue = "X";
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: 
-            GridView.builder(
-              itemCount: 9,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-             itemBuilder: (context, index) {
-              return Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  border: Border.all(color: Colors.black),
-                ),
-              );
-            },
-          
-        ),
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text( 'Tic Tac Toe', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GridView.builder(
+                itemCount: 9,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisSpacing: 2,mainAxisSpacing: 2, crossAxisCount: 3), 
+                  itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    if(boardData[index] == ""){
+                      boardData[index] = lastValue;
+                      changeValue();
+                      setState(() {
+                        
+                      });
+                    }
+                   
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                  decoration: BoxDecoration( color: Colors.blue,),
+                  child: Center(
+                    child: Text(boardData[index],
+                    style: TextStyle(
+                      color: Colors.white,fontSize: 24),)),
+                                ),
+                );
+              },),
+            ),
+          )
+        ],
       ),
     );
   }
