0107a176d0e7124e4ac3ddb9fc13966fa3e3b880
Ranjan Subedi <144402172+Ranjan-subedi@users.noreply.github.com>
2025-12-09 19:33:30 +0545

    color change

--- tic_tac_toe/lib/pages/game_page.dart
+++ tic_tac_toe/lib/pages/game_page.dart
@@ -21,12 +21,20 @@ class _Tic_Tac_ToeState extends State<Tic_Tac_Toe> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Color(0xFF0A0F0D),
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: Text("Tic-Tac-Toe",style: TextStyle(
+            letterSpacing: 2,
+            fontWeight: FontWeight.bold,fontSize: 40),),
+        centerTitle: true,
+        backgroundColor: Color(0xFF091F1B),
+        foregroundColor: Color(0xFF00FFA3),
+      ),
       body: Column(
         // mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text( 'Tic Tac Toe', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
           Container(
             height: 430,
             child: Padding(
@@ -34,7 +42,7 @@ class _Tic_Tac_ToeState extends State<Tic_Tac_Toe> {
               child: GridView.builder(
                 itemCount: 9,
                 physics: NeverScrollableScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisSpacing: 2,mainAxisSpacing: 2, crossAxisCount: 3),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisSpacing: 4,mainAxisSpacing: 4, crossAxisCount: 3),
                 itemBuilder: (context, index) {
                   return GestureDetector(
                     onTap: () {
@@ -50,18 +58,47 @@ class _Tic_Tac_ToeState extends State<Tic_Tac_Toe> {
                     child: Container(
                       height: 100,
                       width: 100,
-                      decoration: BoxDecoration( color: Colors.blue,),
+                      decoration: BoxDecoration( color: Color(0xFF10312B),
+                      borderRadius: BorderRadius.circular(16)
+                      ),
                       child: Center(
                           child: Text(boardData[index],
                             style: TextStyle(
-                                color: Colors.white,fontSize: 24),)),
+                                color: boardData[index]=="X" ? Color(0xFF00FF88): Color(0xFFFF005C),fontSize: 32, fontWeight: FontWeight.bold),)),
                     ),
                   );
                 },),
             ),
           ),
           Divider(),
-          Text("Turn of $lastValue",style: TextStyle(fontSize: 22,color: Colors.black),),
+          Text("Turn of $lastValue",style: TextStyle(fontSize: 28,color: Color(0xFF00FFA3),fontWeight: FontWeight.w600,letterSpacing: 1.5),),
+
+          SizedBox(
+            height: 20,
+          ),
+          Text("Winner",style: TextStyle(
+            fontSize: 18,color: Color(0xFF00FFA3),
+          )),
+          Container(
+            color: Colors.white24,
+            height: 40,
+            width: MediaQuery.of(context).size.width*0.8,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("O 1",style: TextStyle(
+                  fontSize: 18,color: Color(0xFF00FFA3),
+                ),),
+                Text("X 1",style: TextStyle(
+                  fontSize: 18,color: Color(0xFFFF005C),
+
+                ),)
+
+
+              ],
+            ),
+          )
         ],
       ),
     );
